2270f21d86d11b84436392e69afdce87e6195c87
Leghend123 <nelsonagbagah1002@gmail.com>
2024-05-10 14:50:01 +0000

    updated now

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 from flask import Flask, request
 import sqlite3
-from database import CREATE_ROOMS_TABLE
+from database import CREATE_ROOMS_TABLE, CREATE_TEMP_TABLE
+from datetime import datetime, timezone
+
 
 
 app = Flask(__name__)
 
+#creating a room endpoint..............
 @app.post("/api/room")
 def create_room():
     data = request.get_json()
@@ -24,15 +27,39 @@ def create_room():
 
     return{"id": room_id, "message": f"Room {name} created."}, 201
 
-
+#Addinng temperature endpoint.....................
 @app.post("/api/temperature")
-def create_temp():
+def add_temp():
     data = request.get_json()
     room_id = data["room"]
     temperature = data["temperature"]
     
+    try:
+        date = datetime.strptime(data["date"], "%m-%d-%Y %H:%M:%S")
+
+    except KeyError:
+        date = datetime.now(timezone.utc)
+
+    with sqlite3.connect("database.db") as con:
+        cursor = con.cursor()
+        temp_query = "INSERT INTO temperatures(room_id, temperature, date) VALUES(?,?,?)"
+        cursor.execute(temp_query, (room_id, temperature, date))
+
+    return {"message": "Temperature added."}, 201
+
+#Finding Average
+@app.get("/api/average")
+def get_average_temp():
+    with sqlite3.connect("database.db") as con:
+        cursor = con.cursor()
+        Total_Num_Of_Days = """SELECT COUNT(DISTINCT DATE(date)) as days FROM temperatures"""
+        Temp_Avg = """ SELECT AVG(temperature) as average FROM temperatures"""
+        cursor.execute(Temp_Avg)
+        average = cursor.fetchone()[0]
+        cursor.execute(Total_Num_Of_Days)
+        days = cursor.fetchone()[0]
+
+    return {"average": round(average, 2), "days": days}
+
     
-
-
-
 
